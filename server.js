888de090a48ddbdc052121f0c757357f8eb3945a
YogaPan <godhand1234567@gmail.com>
2016-06-15 12:18:23 +0800

    Add socket.io

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-import Koa from 'Koa';
-import views from 'koa-views';
-import logger from 'koa-logger';
-import serve from 'koa-static';
-import bodyParser from 'koa-bodyparser';
-import session from 'koa-session';
-import convert from 'koa-convert';
+const Koa = require('Koa');
+const logger = require('koa-logger');
+const views = require('koa-views');
+const serve = require('koa-static');
+const router = require('./app/router');
+const bodyParser = require('koa-bodyparser');
+const session = require('koa-session');
+const convert = require('koa-convert');
+const mongoose = require('mongoose');
 
-import mongoose from 'mongoose';
-import path from 'path';
-import router from './app/router';
+const path = require('path');
+const http = require('http');
 
-// Connect To mongodb use test db.
-mongoose.connect('mongodb://localhost/test');
-
+// Create a new koa application.
 const app = new Koa;
 
-// Log all request, include status and spended time.
+// Connect to localhost mongodb.
+mongoose.connect('mongodb://localhost/test');
+
+// Show all request, include path, status code and spent time.
 app.use(logger());
 
 // Parse post form as json.
 app.use(bodyParser());
 
-// Use ejs template engine.
+// Use ejs as template engines.
 app.use(views(path.join(__dirname, '/views'), {
   map: { ejs: 'ejs' }
 }));
@@ -29,32 +31,35 @@ app.use(views(path.join(__dirname, '/views'), {
 // Serve static files in "public" directory.
 app.use(serve(path.join(__dirname, '/public')));
 
-// Use koa-session and set secret key.
-app.keys = ['You never know'];
+// Set secret key and use koa-session.
+// NOTE: In product, this secret key MUST CHANGE and add to .gitignore file.
+app.keys = ['You never know.'];
 app.use(convert(session(app)));
 
 // Catch all unhandled server internal errors and display message.
-//app.env = 'product'
+//app.env = 'product';
 app.use(async (ctx, next) => {
   try {
     await next();
-  } catch(err) {
+  } catch (err) {
     ctx.status = err.status || 500;
 
     // Show message to server side.
     console.log(err);
 
     // Show message to client side.
-    // Note: This only enable when in development env.
-    // Get rid of this features, set app.env = 'product'.
-    if ('development' == app.env) {
+    // NOTE: THis only enable when in "development" env.
+    // Get rid of this feature, set app.env = "product".
+    if ('development' === app.env) {
       switch (ctx.accepts('html', 'json')) {
         case 'html':
           ctx.type = 'html';
           ctx.body = `<p>${err.message}</p>`;
+          break;
         case 'json':
           ctx.type = 'json';
           ctx.body = { message: err.message };
+          break;
         default:
           ctx.type = 'text';
           ctx.body = err.message;
@@ -63,38 +68,40 @@ app.use(async (ctx, next) => {
       switch (ctx.accepts('html', 'json')) {
         case 'html':
           ctx.type = 'html';
-          ctx.body = '<p>Server Internal Error.</p>'
+          ctx.body = '<p>Server Internal Error</p>';
+          break;
         case 'json':
           ctx.type = 'json';
-          ctx.body = { message: 'Server Internal Error.' };
+          ctx.body = { message: 'Server Internal Error' };
+          break;
         default:
           ctx.type = 'text';
-          ctx.body = 'Server Internal Error.';
+          ctx.body = 'Server Internal Error';
       }
     }
   }
 });
 
-// 404 Not found page
+// 404 Not Found Page.
 app.use(async (ctx, next) => {
   await next();
 
-  if (ctx.status != 404)
+  if (404 !== ctx.status)
     return;
   ctx.status = 404;
 
   switch (ctx.accepts('html', 'json')) {
     case 'html':
       ctx.type = 'html';
-      ctx.body = '<p>Page Not Found</p>'
+      ctx.body = '<p>404 Page Not Found</p>';
       break;
     case 'json':
       ctx.type = 'json';
-      ctx.body = { message: 'Page Not Found' };
+      ctx.body = { message: '404 Page Not Found' };
       break;
     default:
       ctx.type = 'text';
-      ctx.body = 'Page Not Found';
+      ctx.body = '404 Page Not Found';
   }
 });
 
@@ -103,9 +110,19 @@ app
   .use(router.routes())
   .use(router.allowedMethods());
 
-// Start listening.
+// Start Server listen.
 const port = process.env.PORT || 8080;
+const server = http.Server(app.callback());
 
-app.listen(port, () => {
-  console.log(`Server listen on port ${port}`);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
 });
